95cb7604debe501d2be06b6988c7ac36571507a9
Michael Brooks <mrprovisional@yahoo.com>
2018-07-29 09:58:56 -0400

    really add the tests

---
+++ StockServiceFactory/src/test/java/edu/mbrooks/advanced/model/PersonTest.java
@@ -0,0 +1,57 @@
+package edu.mbrooks.advanced.model;
+
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+/*
+    public  static final Calendar birthDayCalendar = Calendar.getInstance();
+
+    static {
+        birthDayCalendar.set(1963, Calendar.JUNE, 30);
+    }
+*/
+
+    public static final String firstName = "Michael";
+    public  static final String lastName = "Brooks";
+    //public static final Timestamp birthDate = new Timestamp(birthDayCalendar.getTimeInMillis());
+
+    /**
+     * Testing helper method for generating Person test data
+     *
+     * @return a Person object that uses static constants for data.
+     */
+    public static Person createPerson() {
+        Person person = new Person();
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        return person;
+    }
+
+    @Test
+    public void testPersonGettersAndSetters() {
+        Person person = createPerson();
+        int id = 10;
+        person.setId(id);
+        assertEquals("first name matches", firstName, person.getFirstName());
+        assertEquals("last name matches", lastName, person.getLastName());
+        //assertEquals("birthday matches", birthDate, person.getBirthDate());
+        assertEquals("id matches", id, person.getId());
+
+    }
+
+    @Test
+    public void testToString() {
+        Person person = createPerson();
+        System.out.println(person.toString());
+
+        String expected_result = "Person{firstName= 'Michael', lastName= 'Brooks'}";
+        assertEquals("toString()", expected_result, person.toString());
+
+    }
+}
